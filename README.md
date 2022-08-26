71038df5871dbbdf9a176cff77df818ee3ce197f
nutv99 <nutv99@gmail.com>
2022-08-26 18:37:11 +0700

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# angular-ivy-rwwby5
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/angular-ivy-rwwby5)
---
+++ src/app/imgupload/imgupload.component.ts
@@ -0,0 +1,46 @@
+import { Component, OnInit,Input,Output,EventEmitter } from '@angular/core';
+
+@Component({
+  selector: 'app-imgupload',
+  templateUrl: './imgupload.component.html',
+  styleUrls: ['./imgupload.component.css']
+})
+export class ImguploadComponent implements OnInit {
+  @Input() myurlcode = '';
+  @Output() ImguploadComponent = new EventEmitter();
+
+  url: any; 
+	msg = "";
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+  	
+	
+	
+	//selectFile(event) { //Angular 8
+	selectFile(event: any) { //Angular 11, for stricter type
+		if(!event.target.files[0] || event.target.files[0].length == 0) {
+			this.msg = 'You must select an image';
+			return;
+		}
+		
+		var mimeType = event.target.files[0].type;
+		
+		if (mimeType.match(/image\/*/) == null) {
+			this.msg = "Only images are supported";
+			return;
+		}
+		
+		var reader = new FileReader();
+		reader.readAsDataURL(event.target.files[0]);
+		
+		reader.onload = (_event) => {
+			this.msg = "";
+			this.url = reader.result; 
+		}
+	}
+
+}
