8fe487bb8bc4ae9bd5e856d43732eacb4e4516c4
nutv99 <nutv99@gmail.com>
2022-08-27 07:18:21 +0700

    4 files modified

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,20 +1,22 @@
 import { Component, VERSION } from '@angular/core';
-import { ImguploadComponent} from './imgupload/imgupload.component' ;
+import { ImguploadComponent } from './imgupload/imgupload.component';
 
 @Component({
   selector: 'my-app',
   templateUrl: './app.component.html',
-  styleUrls: [ './app.component.css' ]
+  styleUrls: ['./app.component.css'],
 })
-export class AppComponent  {
+export class AppComponent {
   name = 'Angular ' + VERSION.major;
-
+  valueC! : any ;
+  
   //url; //Angular 8
-  displayCounter(count) {
-    console.log(count);
-}
+  displayCounter(count:any) {
+    this.valueC = count ;
+    alert(count);
+  }
 
-displayCounter22(count) {
-  console.log('Data22',count);
-}
+  displayCounter22(count:any) {
+    alert( count);
+  }
 }
